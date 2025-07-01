7c3781419d03b9d947ca91a07a2bc0478dcf7758
Prompt-Bit <phawach4prompt@gmail.com>
2025-07-01 18:40:34 +0700

    Create main.cpp

---
+++ admin/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Protection Enabled" << std::endl;
+    std::cin;
+    std::cout << "Welcome to your protector" << std::endl;
+    
+    return 0;
+}
