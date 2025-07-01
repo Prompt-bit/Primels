6104ae9552a8aa106022d2e440998b3ef07fc4a8
Prompt-Bit <phawach4prompt@gmail.com>
2025-07-01 18:49:34 +0700

    Update main.cpp

--- admin/main.cpp
+++ admin/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include "protect.h"
 
 int main()
 {
     std::cout << "Protection Enabled" << std::endl;
     std::cin;
-    std::cout << "Welcome to your protector" << std::endl;
+    std::cout << "Welcome to your protector enter your feature" << std::endl;
+    std::cin;
+
+    int protect(int 1, int 3) {
+        return 1 + 3;
+    }
     
     return 0;
 }
